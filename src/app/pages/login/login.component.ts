19e9219c9338862a4029523f1dd1e92c9f3fcf15
Alfredo Ruiz <rruiz@tsspe.com>
2024-10-15 00:05:50 -0500

    Custom form field is removed

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BaseComponent } from '@components/base-component';
-import { FormControlModule } from '@modules/form-control.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormControlModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
