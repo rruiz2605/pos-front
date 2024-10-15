4b38cb966418b63433fc04d262ab617e8f47650b
Alfredo Ruiz <rruiz@tsspe.com>
2024-10-14 23:57:27 -0500

    Angular Material is installed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideHttpClient(
       withFetch()
-    )
+    ), provideAnimationsAsync()
   ]
 };
